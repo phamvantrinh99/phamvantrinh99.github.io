77f7886742d2be538ccebb02b75ee37572bf216f
phamvantrinh99 <trinh.pham@niteco.se>
2025-06-30 11:35:43 +0700

    remove redundant code

--- TraditionalMedicalClinic.Business/Constants/PageIconPaths.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TraditionalMedicalClinic.Business.Constants
-{
-    public static class PageIconPaths
-    {
-        public const string HomeIcon = "/icons/cms/pages/CMS-icon-page-02.png";
-    }
-}
